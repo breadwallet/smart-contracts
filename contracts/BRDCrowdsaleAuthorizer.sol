4aa032a22b716d9e7796cd436aa0c476e9ce5840
Samuel Sutch <sam@sutch.net>
2017-12-03 00:55:45 -0800

    use Ownable for BRDCrowdsaleAuthorizer

--- contracts/BRDCrowdsaleAuthorizer.sol
+++ contracts/BRDCrowdsaleAuthorizer.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.15;
 
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
 
 /**
  * Contract BRDCrowdsaleAuthorizer is used by the crowdsale website
@@ -7,35 +9,25 @@ pragma solidity ^0.4.15;
  * participants must go through the KYC/AML phase, only accounts
  * listed in this contract may contribute to the crowdsale
  */
-contract BRDCrowdsaleAuthorizer {
+contract BRDCrowdsaleAuthorizer is Ownable {
   // these accounts are authorized to participate in the crowdsale
   mapping (address => bool) internal authorizedAccounts;
   // these accounts are authorized to authorize accounts
   mapping (address => bool) internal authorizers;
-  // this address created the authorizer contract
-  address internal contractCreator;
 
   // emitted when a new account is authorized
   event Authorized(address indexed _to);
 
-  // constructor
-  function BRDCrowdsaleAuthorizer(address _initialAuthorzer)  public {
-    // retain the contract creator
-    contractCreator = msg.sender;
-    // retain the initial authorizer
-    authorizers[_initialAuthorzer] = true;
-  }
-
   // add an authorizer to the authorizers mapping. the _newAuthorizer will
   // be able to add other authorizers and authorize crowdsale participants
-  function addAuthorizer(address _newAuthorizer) onlyCreatorOrAuthorizer() public {
+  function addAuthorizer(address _newAuthorizer) onlyOwnerOrAuthorizer public {
     // allow the provided address to authorize accounts
     authorizers[_newAuthorizer] = true;
   }
 
   // remove an authorizer from the authorizers mapping. the _bannedAuthorizer will
   // no longer have permission to do anything on this contract
-  function removeAuthorizer(address _bannedAuthorizer) onlyCreatorOrAuthorizer() public {
+  function removeAuthorizer(address _bannedAuthorizer) onlyOwnerOrAuthorizer public {
     // only attempt to remove the authorizer if they are currently authorized
     require(authorizers[_bannedAuthorizer]);
     // remove the authorizer
@@ -43,7 +35,7 @@ contract BRDCrowdsaleAuthorizer {
   }
 
   // allow an account to participate in the crowdsale
-  function authorizeAccount(address _newAccount) onlyCreatorOrAuthorizer() public {
+  function authorizeAccount(address _newAccount) onlyOwnerOrAuthorizer public {
     if (!authorizedAccounts[_newAccount]) {
       // allow the provided account to participate in the crowdsale
       authorizedAccounts[_newAccount] = true;
@@ -54,7 +46,7 @@ contract BRDCrowdsaleAuthorizer {
 
   // returns whether or not the provided _account is an authorizer
   function isAuthorizer(address _account) constant public returns (bool _isAuthorizer) {
-    return authorizers[_account] == true;
+    return msg.sender == owner || authorizers[_account] == true;
   }
 
   // returns whether or not the provided _account is authorized to participate in the crowdsale
@@ -63,8 +55,8 @@ contract BRDCrowdsaleAuthorizer {
   }
 
   // allow only the contract creator or one of the authorizers to do this
-  modifier onlyCreatorOrAuthorizer() {
-    require(msg.sender == contractCreator || authorizers[msg.sender]);
+  modifier onlyOwnerOrAuthorizer() {
+    require(msg.sender == owner || authorizers[msg.sender]);
     _;
   }
 }
